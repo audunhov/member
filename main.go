8bb5d95436fbe8eff1f20275db3c1161a47d2f55
Audun Hammer Hovda <hammerhovda@gmail.com>
2026-01-16 13:46:59 +0100

    postgres implemented

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+	"os"
+
+	"github.com/jackc/pgx/v5"
 )
 
 func main() {
+	dbUrl := os.Getenv("DATABASE_URL")
+	fmt.Println("URL:",dbUrl)
+	conn, err := pgx.Connect(context.Background(), dbUrl)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello world"))
